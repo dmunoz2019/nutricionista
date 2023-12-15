7cae7892fe30dfcb79c77a52672412f420254f8c
Diego Alejandro Muñoz del rio <50757943+dmunoz2019@users.noreply.github.com>
2023-12-15 15:17:55 -0600

    Update sale_order.py

--- model/sale_order.py
+++ model/sale_order.py
@@ -1,6 +1,6 @@
-# En tu archivo models/sale_order.py
-
 from odoo import models, fields, api
+from datetime import timedelta
+import datetime
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,24 +10,41 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, vals):
         res = super(SaleOrder, self).create(vals)
-        res.has_medical_service = res._check_medical_service()
+        has_medical_service = res._check_medical_service()
+        res.write({'has_medical_service': has_medical_service})
+        if has_medical_service:
+            res.create_medical_appointment()
         return res
+
+    def find_available_doctor(self, desired_date, duration_hours=1):
+        doctors = self.env['medical.physician'].search([])
+
+        for doctor in doctors:
+            appointments = self.env['medical.appointment'].search([
+                ('doctor_id', '=', doctor.id),
+                ('appointment_date', '>=', desired_date),
+                ('appointment_date', '<=', desired_date + timedelta(hours=duration_hours))
+            ])
+
+            if not appointments:
+                return doctor
+        return None
 
     def _check_medical_service(self):
         return any(line.product_id.is_medical and line.product_id.type == 'service' for line in self.order_line)
 
     def create_medical_appointment(self):
-        # Lógica para crear una cita médica
         if self.has_medical_service:
-            for line in self.order_line:
-                if line.product_id.is_medical and line.product_id.type == 'service':
-                    # Crear registro en medical.appointment aquí
-                    self.env['medical.appointment'].create({
-                        'patient_id': self.partner_id.id,  # Asumiendo que el cliente es el paciente
-                        'appointment_date': fields.Date.today(),
-                        'doctor_id' : 1,
-                    })
-                    break
+            desired_date = self.date_order if isinstance(self.date_order, datetime.datetime) else fields.Datetime.from_string(self.date_order)
+            available_doctor = self.find_available_doctor(desired_date)
+
+            if available_doctor:
+                self.env['medical.appointment'].create({
+                    'patient_id': self.partner_id.id,
+                    'doctor_id': available_doctor.id,
+                    'appointment_date': desired_date
+                })
+
     def action_view_medical_appointments(self):
         self.ensure_one()
         return {
