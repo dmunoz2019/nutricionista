ad1d26d66b9c99d6f2eee2a20ece803137871d4d
Diego Alejandro Muñoz del rio <50757943+dmunoz2019@users.noreply.github.com>
2023-12-15 13:22:52 -0600

    Update medical_patient_evaluation.py

--- model/medical_patient_evaluation.py
+++ model/medical_patient_evaluation.py
@@ -237,56 +237,56 @@ class medical_patient_evaluation(models.Model):
 	secondary_conditions_ids = fields.One2many('medical.secondary_condition','patient_evaluation_id','Secondary Conditions')
 	diagnostic_hypothesis_ids = fields.One2many('medical.diagnostic_hypotesis','patient_evaluation_id','Procedures')
 	procedure_ids = fields.One2many('medical.directions','patient_evaluation_id','Procedures')
-	evaluation_sale_order_lines = fields.One2many('evaluation.order.line','evaluation_id','Lineas de venta')
+	# evaluation_sale_order_lines = fields.One2many('evaluation.order.line','evaluation_id','Lineas de venta')
 
 	payment_term_id = fields.Many2one('account.payment.term',string="Payment Term")
 	
-	sale_count = fields.Integer(compute='_compute_sale_count', string='Sale Order Count')
-	def confirm_session_sale(self):
-		self.ensure_one()
-		validity_date = self.start_evaluation + timedelta(days=1)
-		sale_order_vals = {
-			'partner_id': self.patient_id.id,
-			'order_line': [(6, 0, self.evaluation_sale_order_lines.ids)],
-			'validity_date':  validity_date,
-			'date_order': self.start_evaluation.date(),
-			'payment_term_id': self.payment_term_id.id if self.payment_term_id else 1,
-			'user_id': self.env.user.id,
-			'tag_ids': [(6, 0, self.patient_id.tag_ids.ids)],
-			'oringin': 'Appointment'.concat(self.medical_appointment_date_id.name) if self.medical_appointment_date_id else 'Evaluation'.concat(self.name)}
-		sale_order = self.env['sale.order'].create(sale_order_vals)
-		return {
-			'name': _('Sale Order'),
-			'view_type': 'form',
-			'view_mode': 'form',
-			'res_model': 'sale.order',
-			'type': 'ir.actions.act_window',
-			'res_id': sale_order.id,
-			'context': self.env.context,
-		}
+	# sale_count = fields.Integer(compute='_compute_sale_count', string='Sale Order Count')
+	# def confirm_session_sale(self):
+	# 	self.ensure_one()
+	# 	validity_date = self.start_evaluation + timedelta(days=1)
+	# 	sale_order_vals = {
+	# 		'partner_id': self.patient_id.id,
+	# 		'order_line': [(6, 0, self.evaluation_sale_order_lines.ids)],
+	# 		'validity_date':  validity_date,
+	# 		'date_order': self.start_evaluation.date(),
+	# 		'payment_term_id': self.payment_term_id.id if self.payment_term_id else 1,
+	# 		'user_id': self.env.user.id,
+	# 		'tag_ids': [(6, 0, self.patient_id.tag_ids.ids)],
+	# 		'oringin': 'Appointment'.concat(self.medical_appointment_date_id.name) if self.medical_appointment_date_id else 'Evaluation'.concat(self.name)}
+	# 	sale_order = self.env['sale.order'].create(sale_order_vals)
+	# 	return {
+	# 		'name': _('Sale Order'),
+	# 		'view_type': 'form',
+	# 		'view_mode': 'form',
+	# 		'res_model': 'sale.order',
+	# 		'type': 'ir.actions.act_window',
+	# 		'res_id': sale_order.id,
+	# 		'context': self.env.context,
+	# 	}
  
-	@api.depends('evaluation_sale_order_lines')
-	def _compute_sale_count(self):
-		for record in self:
-			record.sale_count = len(record.evaluation_sale_order_lines)
+	# @api.depends('evaluation_sale_order_lines')
+	# def _compute_sale_count(self):
+	# 	for record in self:
+	# 		record.sale_count = len(record.evaluation_sale_order_lines)
 
-class EvaluationOrderLine(models.Model):
-    _name = 'evaluation.order.line'
-    _inherits = 'sale.order.line'
-    _description = 'Evaluation Order Line'
+# class EvaluationOrderLine(models.Model):
+#     _name = 'evaluation.order.line'
+#     _inherits = 'sale.order.line'
+#     _description = 'Evaluation Order Line'
 
-    evaluation_id = fields.Many2one(
-        comodel_name='medical.patient.evaluation',  # Reemplaza con el nombre real de tu modelo de evaluación
-        string='Evaluation Reference',
-        required=True,
-        ondelete='cascade',
-        index=True,
-        copy=False
-    )
+#     evaluation_id = fields.Many2one(
+#         comodel_name='medical.patient.evaluation',  # Reemplaza con el nombre real de tu modelo de evaluación
+#         string='Evaluation Reference',
+#         required=True,
+#         ondelete='cascade',
+#         index=True,
+#         copy=False
+#     )
 
-    @api.depends('product_id', 'evaluation_id')
-    def _compute_name(self):
-        super(EvaluationOrderLine, self)._compute_name()
-        for record in self:
-            if record.evaluation_id:
-                record.name = f"{record.evaluation_id.name} - {record.product_id.name}"
+#     @api.depends('product_id', 'evaluation_id')
+#     def _compute_name(self):
+#         super(EvaluationOrderLine, self)._compute_name()
+#         for record in self:
+#             if record.evaluation_id:
+#                 record.name = f"{record.evaluation_id.name} - {record.product_id.name}"
